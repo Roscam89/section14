4b6ffa143523977f3f9293d77533227a1786ac3c
max <lil_max2002@yahoo.com>
2022-10-13 21:13:30 -0700

    changes 	modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const Person = function (firsName, birthYear, job) {
+  this.firsName = firsName;
+  this.birthYear = birthYear;
+  this.job = job;
+};
+
+const max = new Person('Maxim', 1989, 'truck driver');
+console.log(max);
